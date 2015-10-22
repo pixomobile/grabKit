ab0371b2c2e7eb223dbafacde8a8ae8b2e743219
PeterWong2015 <peter9981@icloud.com>
2015-10-22 07:10:41 -0700

    update podspec file

--- grabKit/grabKit/GrabKitLib/serviceGrabbers/facebookGrabber/GRKFacebookConnector.m
+++ grabKit/grabKit/GrabKitLib/serviceGrabbers/facebookGrabber/GRKFacebookConnector.m
@@ -1,20 +1,20 @@
 /*
  * This file is part of the GrabKit package.
  * Copyright (c) 2013 Pierre-Olivier Simonard <pierre.olivier.simonard@gmail.com>
- *  
- * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and 
- * associated documentation files (the "Software"), to deal in the Software without restriction, including 
- * without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell 
- * copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the 
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+ * associated documentation files (the "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the
  * following conditions:
- *  
- * The above copyright notice and this permission notice shall be included in all copies or substantial 
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial
  * portions of the Software.
- *  
- * The Software is provided "as is", without warranty of any kind, express or implied, including but not 
+ *
+ * The Software is provided "as is", without warranty of any kind, express or implied, including but not
  * limited to the warranties of merchantability, fitness for a particular purpose and noninfringement. In no
  * event shall the authors or copyright holders be liable for any claim, damages or other liability, whether
- * in an action of contract, tort or otherwise, arising from, out of or in connection with the Software or the 
+ * in an action of contract, tort or otherwise, arising from, out of or in connection with the Software or the
  * use or other dealings in the Software.
  *
  * Except as contained in this notice, the name(s) of (the) Author shall not be used in advertising or otherwise
@@ -49,7 +49,7 @@ static NSString * expirationDateKey = @"ExpirationDateKey";
         
         _isConnecting = NO;
         
-    }     
+    }
     
     return self;
 }
@@ -63,20 +63,20 @@ static NSString * expirationDateKey = @"ExpirationDateKey";
     connectionIsCompleteBlock = completeBlock;
     connectionDidFailBlock = errorBlock;
     
-    // The Facebook SDK keeps internal values allowing to test, at any moment, if the session is valid or not.   
+    // The Facebook SDK keeps internal values allowing to test, at any moment, if the session is valid or not.
     if ( ! [FBSDKAccessToken currentAccessToken] ) {
         
         
         [[GRKConnectorsDispatcher sharedInstance] registerServiceConnectorAsConnecting:self];
         _applicationDidEnterBackground = NO;
-    
-        FBSDKLoginManager *login = [[FBSDKLoginManager alloc] init];
-        NSArray * permissions = @[@"user_photos", @"user_photo_video_tags"];
         
-        [login logInWithReadPermissions:permissions handler:^(FBSDKLoginManagerLoginResult *result, NSError *error) {
+        FBSDKLoginManager *login = [[FBSDKLoginManager alloc] init];
+        NSArray * permissions = @[@"public_profile",@"user_photos", @"user_tagged_places"];
+        
+        [login logInWithReadPermissions:permissions fromViewController:nil handler:^(FBSDKLoginManagerLoginResult *result, NSError *error) {
             if (error) {
                 // Process error
-                 errorBlock(error);
+                errorBlock(error);
             } else if (result.isCancelled) {
                 // Handle cancellations
                 completeBlock(NO);
@@ -93,39 +93,39 @@ static NSString * expirationDateKey = @"ExpirationDateKey";
     } else  {
         
         GRKFacebookQuery * query = nil;
-        query = [GRKFacebookQuery queryWithGraphPath:@"me" 
-                                         withParams:nil 
-                                  withHandlingBlock:^(GRKFacebookQuery *query, id result) {
-        
-                                      // Store the locale
-                                      [GRKFacebookSingleton sharedInstance].userLocale = [result objectForKey:@"locale"];
-                                      
-                                      if (completeBlock != nil ){
-    	                                  completeBlock(YES);
-	                                  }
-                                      
-                                      [_queries removeObject:query];
-                                      
-                                  } andErrorBlock:^(NSError *error) {
-                                      
-                                     	// if we got an error trying to make a basic query, 
-                                    	//  but as the session is supposed to be valid, 
+        query = [GRKFacebookQuery queryWithGraphPath:@"me"
+                                          withParams:nil
+                                   withHandlingBlock:^(GRKFacebookQuery *query, id result) {
+                                       
+                                       // Store the locale
+                                       [GRKFacebookSingleton sharedInstance].userLocale = [result objectForKey:@"locale"];
+                                       
+                                       if (completeBlock != nil ){
+                                           completeBlock(YES);
+                                       }
+                                       
+                                       [_queries removeObject:query];
+                                       
+                                   } andErrorBlock:^(NSError *error) {
+                                       
+                                       // if we got an error trying to make a basic query,
+                                       //  but as the session is supposed to be valid,
                                       	// Then the user may have removed the application on Facebook.
-                                  		
-                                      // then, remove the store data about the session
-                                      [GRKTokenStore removeTokenWithName:accessTokenKey forGrabberType:grabberType];
-                                      [GRKTokenStore removeTokenWithName:expirationDateKey forGrabberType:grabberType];
-
-                                      errorBlock(error);
-                                      
-                                      [_queries removeObject:query];
-                                  
-                                  }];
-
+                                       
+                                       // then, remove the store data about the session
+                                       [GRKTokenStore removeTokenWithName:accessTokenKey forGrabberType:grabberType];
+                                       [GRKTokenStore removeTokenWithName:expirationDateKey forGrabberType:grabberType];
+                                       
+                                       errorBlock(error);
+                                       
+                                       [_queries removeObject:query];
+                                       
+                                   }];
+        
         [_queries addObject:query];
         [query perform];
-
-            
+        
+        
         
     }
     
@@ -135,7 +135,7 @@ static NSString * expirationDateKey = @"ExpirationDateKey";
  */
 -(void)disconnectWithDisconnectionIsCompleteBlock:(GRKGrabberDisconnectionIsCompleteBlock)completeBlock andErrorBlock:(GRKErrorBlock)errorBlock;
 {
-   
+    
     [[[FBSDKLoginManager alloc] init] logOut];
     
     [self isConnected:^(BOOL connected) {
@@ -165,7 +165,7 @@ static NSString * expirationDateKey = @"ExpirationDateKey";
 -(void) isConnected:(GRKGrabberConnectionIsCompleteBlock)connectedBlock errorBlock:(GRKErrorBlock)errorBlock;
 {
     if ( ! [FBSDKAccessToken currentAccessToken] ){
-    
+        
         dispatch_async_on_main_queue(connectedBlock, NO);
         
         return;
@@ -176,10 +176,12 @@ static NSString * expirationDateKey = @"ExpirationDateKey";
     
     
     __weak __block GRKFacebookQuery * query = nil;
-    query = [GRKFacebookQuery queryWithGraphPath:@"me?fields=locale" // let's ask for a restricted set of data ...
-                                      withParams:nil
+    NSMutableDictionary* localeParameter = [NSMutableDictionary dictionary];
+    [localeParameter setValue:@"locale"  forKey:@"fields"];
+    query = [GRKFacebookQuery queryWithGraphPath:@"me" // let's ask for a restricted set of data ...
+                                      withParams:localeParameter
                                withHandlingBlock:^(GRKFacebookQuery *query, id result) {
-
+                                   
                                    // Store the locale
                                    [GRKFacebookSingleton sharedInstance].userLocale = [result objectForKey:@"locale"];
                                    
@@ -191,7 +193,7 @@ static NSString * expirationDateKey = @"ExpirationDateKey";
                                    query = nil;
                                    
                                } andErrorBlock:^(NSError *error) {
-                    
+                                   
                                    if (connectedBlock != nil ){
                                        connectedBlock(NO);
                                    }
@@ -203,7 +205,7 @@ static NSString * expirationDateKey = @"ExpirationDateKey";
     
     [_queries addObject:query];
     [query perform];
-
+    
     
 }
 
@@ -216,26 +218,26 @@ static NSString * expirationDateKey = @"ExpirationDateKey";
 /*  @see refer to GRKServiceConnectorProtocol documentation
  */
 -(void) didNotCompleteConnection;{
-   
-    /*
-        this method is called when the app becomes active.
-        this code below needs to be performed only if the app entered background first.
-        The app can "become active" without entering background first in one peculiar case :
-            When de FB sdk attempts to log in from ACAccountStore, an UIAlertView is displayed 
-            to ask the user if he allows to give access to his FB account.
-            Whether the users allows or refuses, the [UIApplicationDelegate applicationDidBecomeActive] 
-            method is called when the UIAlertView dissmisses.
-     
-    */
-
-    if ( _applicationDidEnterBackground ){
     
+    /*
+     this method is called when the app becomes active.
+     this code below needs to be performed only if the app entered background first.
+     The app can "become active" without entering background first in one peculiar case :
+     When de FB sdk attempts to log in from ACAccountStore, an UIAlertView is displayed
+     to ask the user if he allows to give access to his FB account.
+     Whether the users allows or refuses, the [UIApplicationDelegate applicationDidBecomeActive]
+     method is called when the UIAlertView dissmisses.
+     
+     */
+    
+    if ( _applicationDidEnterBackground ){
+        
         if (connectionIsCompleteBlock != nil ){
             dispatch_async(dispatch_get_main_queue(), ^{
                 connectionIsCompleteBlock(NO);
                 connectionIsCompleteBlock = nil;
             });
-        
+            
         }
     }
     
